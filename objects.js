c2c570efd2067f68a71ea6e38ad161aacf41a3fd
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-12 20:54:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,18 +5,11 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle }
-  );
+  playlist[artistName] = songTitle
+  return playlist
 }
-
 
 function removeFromPlaylist(playlist, artistName) {
  delete playlist[artistName]
  return playlist
-}
-
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
 }
